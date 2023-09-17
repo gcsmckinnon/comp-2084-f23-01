4da444d1743fb56529456bfaf04d875ebddc9e43
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-09-17 13:17:19 -0400

    Created models with data annotations

---
+++ Models/Product.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldDominion.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; } = 0;
+
+        [Required]
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; } = 0;
+
+        [Required, StringLength(300)]
+        public string Name { get; set; } = String.Empty;
+
+        [StringLength(1000)]
+        public string? Description { get; set; } = String.Empty;
+
+        [StringLength(250)]
+        public string? Image { get; set; } = String.Empty;
+
+        [Required]
+        [Range(0.01, 999999.99)]
+        [DataType(DataType.Currency)]
+        public decimal MSRP { get; set; } = 0.01M;
+
+        [ForeignKey("DepartmentId")]
+        public virtual Department? Department { get; set; }
+    }
+}
