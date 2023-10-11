7d9ee5710a5405ef53053b44d8a85aa71e5f032e
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-10-11 13:56:26 -0400

    Added some fixes

--- Models/Product.cs
+++ Models/Product.cs
@@ -36,7 +36,7 @@ namespace WorldDominion.Models
         public decimal MSRP { get; set; } = 0.01M;
 
         [Required]
-        [Range(0.01, 999999.99)]
+        // [Range(0.01, 999999.99)]
         public decimal Weight { get; set; }
 
         [Required]
